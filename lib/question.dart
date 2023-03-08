93a74790ed9627bc5568e4006c97570ddf4e01c7
programmer <abdoulrasacknana86@gmail.com>
2023-03-08 19:28:20 +0000

    Add assets, class and datas

---
+++ lib/question.dart
@@ -0,0 +1,15 @@
+class Question {
+  String question;
+  bool reponse;
+  String explication;
+  String imagePath;
+
+  Question({
+    required this.question,
+    required this.reponse,
+    required this.explication,
+    required this.imagePath,
+  });
+
+  String getImage() => 'assets/images/$imagePath';
+}
